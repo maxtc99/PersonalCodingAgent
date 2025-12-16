4bab0b493514eb6fef865b28e67445ff920a5de5
maxtc99 <max.tc99@gmail.com>
2025-12-16 15:36:21 +0000

    Fixed code

--- test_get_file_content.py
+++ test_get_file_content.py
@@ -4,7 +4,7 @@ def test():
     print(f"Contents of 'main.py' file:\n{get_file_content("calculator", "main.py")}\n")
     print(f"Contents of 'pkg/calculator.py' file:\n{get_file_content("calculator", "pkg/calculator.py")}\n")
     print(f"Contents of '/bin/cat' file:\n{get_file_content("calculator", "/bin/cat")}\n")
-    print(f"Contents of 'pkg/does_not_exist.py' file:\n{get_file_content("calculator", "pkg/does_not_exist.py")}\n")
+    print(f"Contents of 'pkg/does_not_exist.py' fil:\n{get_file_content("calculator", "pkg/does_not_exist.py")}\n")
 
 if __name__ == "__main__":
     test()
--- test_write_file.py
+++ test_write_file.py
@@ -1,7 +1,7 @@
-from functions.write_file import write_file
+from functions.write_file_content import write_file
 
 def test():
-    print(f"Writing to 'lorem.txt' file:\n{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}\n")
+    print(f"Writing to 'lorem.txt' file:\n{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsu")}\n")
     print(f"Writing to 'pkg/morelorem.txt' file:\n{write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}\n")
     print(f"Writing to '/tmp/temp.txt' file:\n{write_file("calculator", "/tmp/temp.txt", "this should not be allowed")}\n")
 
